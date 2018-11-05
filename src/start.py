84e8f00ea7ae0f0be98e4b5c795d58555417fdf8
kukku723 <z03177279@gmail.com>
2018-11-05 14:35:54 +0900

    [update]start rank処理追加

--- src/start.py
+++ src/start.py
@@ -1,7 +1,7 @@
 # /usr/bin/env python
 # -*- coding: utf-8 -*-
-from src.mysql_dao import Dao
-from src.get_rakuten_info  import Genrel_info, Rank_info
+from mysql_dao import Dao
+from get_rakuten_info import Genrel_info, Rank_info
 import logging
 import time
 
@@ -10,7 +10,7 @@ logger = logging.getLogger("log")
 logging.basicConfig(filename="debug.log", filemode='a', level=logging.DEBUG, \
                     format='%(asctime)s:%(levelname)s:%(message)s')
 sql = Dao()
-test=open("./category.md","a",encoding="utf-8")
+test=open("./categorytree/category.md","a",encoding="utf-8")
 
 # 親ジャンル取得
 def genre_insert(noname_lis=[]):
@@ -26,24 +26,53 @@ def genre_insert(noname_lis=[]):
         genre_insert(first_list)
 
     else:
+        #受け取ったリスト引き数の２番のリストを繰り返し処理
         for x in noname_lis[2]:
+            #受け取ったリスト　階層を確認、４階層以下であれば処理続行
             if x[3] < 4 :
                 print(noname_lis)
+
+                #カテゴリーの作成
                 test.write("    " * (x[3] - 1) + "* " + x[1] + "\n")
+                #DBジャンルテーブルへのインサート
+                sql.insert_tabl_genre1(genre_id=x[0],genre_name=x[1])
+                #rank情報インサート
+                rank_insert(genre_id=x[0])
+
                 if x[3]<3:
                     genrel_dict = Genrel_info().get_genrel(x[0])
                     for y in genrel_dict["children"]:
                         id =y["child"]["genreId"]
                         name = y["child"]["genreName"]
                         level = y["child"]["genreLevel"]
-                        print(level)
-                        print(name)
                         noname_lis3 = [id, name, [],level]
                         x[2].append(noname_lis3)
                     genre_insert(x)
                     time.sleep(0.1)
             else:
                 return
+
+def rank_insert(genre_id):
+    # ランク取得
+    rank_info=Rank_info().get_rank(genrel_id=genre_id)
+    time.sleep(0.5)
+    if 'error' in rank_info:
+        return
+    print(rank_info)
+    print("#1"+ str(rank_info["Items"]))
+    print("#2"+ str(rank_info["title"]))
+    print("#3"+ str(rank_info["lastBuildDate"]))
+
+    for x in rank_info["Items"]:
+        name=x["Item"]["itemName"]
+        rank=x["Item"]["rank"]
+        if rank <11:
+            sql.inser_tabl_rank(name=name,rank=rank,genre=genre_id)
+
+
+
+#再帰処理開始
 genre_insert()
 
+#カテゴリーファイルをクローズ
 test.close()
