c5f2eaff2afc04d4e7745eebe25266888f1de1eb
kukku723 <z03177279@gmail.com>
2018-11-10 13:51:36 +0900

    [update]start.py リファクタリング修正

--- src/start.py
+++ src/start.py
@@ -5,6 +5,16 @@ import mysql_dao
 import logging
 import time
 
+##API設定
+#楽天API利用ID
+api_id="xxx"
+#ジャンルサーチAPIURL
+api_url="https://app.rakuten.co.jp/services/api/IchibaGenre/Search/20140222"
+#ランキングサーチAPIURL
+api_rank_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628"
+
+
+#ログ設定
 # loggerの設定
 logger = logging.getLogger("log")
 logging.basicConfig(filename="debug.log", filemode='a', level=logging.INFO, \
@@ -15,7 +25,7 @@ test=open("./category.md","a",encoding="utf-8")
 # 親ジャンル取得
 def genre_insert(noname_lis=[]):
     if noname_lis == []:
-        genrel_dict = get_rakuten_info.Genrel_info().get_genrel(0)
+        genrel_dict = get_rakuten_info.Genrel_info(api_id=api_id,api_url=api_url).get_genrel(0)
         first_list = [0, "category", [],0]
         for x in genrel_dict["children"]:
             id = x["child"]["genreId"]
@@ -39,7 +49,7 @@ def genre_insert(noname_lis=[]):
                 print(x)
 
                 if x[3]<3:
-                    genrel_dict = get_rakuten_info.Genrel_info().get_genrel(x[0])
+                    genrel_dict = get_rakuten_info.Genrel_info(api_id=api_id,api_url=api_url).get_genrel(x[0])
                     for y in genrel_dict["children"]:
                         id =y["child"]["genreId"]
                         name = y["child"]["genreName"]
@@ -53,7 +63,7 @@ def genre_insert(noname_lis=[]):
 
 def rank_insert(genre_id):
     # ランク取得
-    rank_info=get_rakuten_info.Rank_info().get_rank(genrel_id=genre_id)
+    rank_info=get_rakuten_info.Rank_info(api_id=api_id,api_url=api_rank_url).get_rank(genrel_id=genre_id)
     time.sleep(0.5)
     if 'error' in rank_info:
         return
